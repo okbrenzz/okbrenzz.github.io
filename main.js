ebb84a86c5f784e1dfbb971268af098c20de63ea
okbrenzz <143738415+okbrenzz@users.noreply.github.com>
2023-09-29 21:55:59 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const menu = document.querySelector("#menu");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
+const salirads = document.querySelector(".cerrar-ads");
+const ads2 = document.querySelector(".ads2");
+const salirads3 = document.querySelector(".cerrar-ads3");
+const ads3 = document.querySelector(".ads3");
 
 abrir.addEventListener("click", () => {
     menu.classList.add("visible");
@@ -8,4 +12,12 @@ abrir.addEventListener("click", () => {
 
 cerrar.addEventListener("click", () => {
     menu.classList.remove("visible");
+})
+
+salirads.addEventListener("click", () => {
+    ads2.classList.add("hiden");
+})
+
+salirads3.addEventListener("click", () => {
+    ads3.classList.add("hiden");
 })
